a3b35815ac5789dc727e22cfd5f543270b04b129
𓉳 𝗠𝗥 𓋹 𝐏𝐫𝐢𝐦𝐞 ᴋʜᴀʟᴇᴅ𓃵 <funsho318@gmail.com>
2026-06-10 21:46:29 +0100

    Create Menu.js

---
+++ Menu.js
@@ -0,0 +1,14 @@
+module.exports = async (sock, chat) => {
+await sock.sendMessage(chat,{
+text:`🎮 TENSURA RPG BOT
+
+.menu
+.profile
+.pull
+.pull10
+.inventory
+.battle @user
+.boss
+.fuse`
+});
+};
